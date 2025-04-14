3b06555754b571b294fe3080d441996196e907ed
Pedro Martineli <martinelipedro@gmail.com>
2025-04-14 18:16:12 -0300

    feat: add auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,57 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import KeycloakProvider from "next-auth/providers/keycloak";
+import { NextRequest, NextResponse } from "next/server";
+
+console.log("CLIENT_ID:", process.env.KEYCLOAK_CLIENT_ID);
+console.log("CLIENT_SECRET:", process.env.KEYCLOAK_CLIENT_SECRET);
+console.log("ISSUER:", process.env.KEYCLOAK_ISSUER);
+console.log("SECRET:", process.env.NEXTAUTH_SECRET);
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    KeycloakProvider({
+      clientId: process.env.KEYCLOAK_CLIENT_ID!,
+      clientSecret: process.env.KEYCLOAK_CLIENT_SECRET!,
+      issuer: process.env.KEYCLOAK_ISSUER!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async session({ session, token, user }) {
+      if (token.sub) {
+        session.user!.id = token.sub;
+      }
+      if (token.realm_access?.roles) {
+        session.user!.roles = token.realm_access.roles;
+      }
+      return session;
+    },
+    async jwt({ token, account, profile }) {
+      if (account) {
+        token.access_token = account.access_token;
+        token.id_token = account.id_token;
+        token.expires_at = account.expires_at;
+      }
+      if (profile) {
+        token.sub = profile?.sub;
+        token.name = profile?.name;
+        token.email = profile?.email;
+      }
+      return token;
+    },
+  },
+  // pages: {
+  //   signIn: "/auth/signin", // Optional custom sign-in page
+  //   error: "/auth/error", // Optional custom error page
+  // },
+};
+
+const handler = NextAuth(authOptions);
+
+export async function GET(req: NextRequest, res: NextResponse) {
+  return handler(req, res) as Promise<NextResponse>;
+}
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  return handler(req, res) as Promise<NextResponse>;
+}
---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,31 @@
+// types/next-auth.d.ts
+import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
+
+declare module "next-auth" {
+  interface User extends DefaultUser {
+    id: string;
+    roles?: string[];
+    preferred_username?: string; // Add the preferred_username property
+    // Add other custom user properties here
+  }
+
+  interface Session extends DefaultSession {
+    user?: User;
+    id_token?: string; // If you're storing the ID token in the session
+    // Add other custom session properties here
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    sub?: string;
+    access_token?: string;
+    id_token?: string;
+    expires_at?: number;
+    roles?: string[];
+    preferred_username?: string; // Add the preferred_username property to JWT as well
+    realm_access?: {
+      roles: string[];
+    };
+  }
+}
